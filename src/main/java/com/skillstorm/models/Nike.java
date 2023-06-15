2463cf0b2902373835aa1724014ce2323b5dba0a
scmalloy <127246669+scmalloy@users.noreply.github.com>
2023-06-15 09:07:36 -0700

    Update Nike.java

--- src/main/java/com/skillstorm/models/Nike.java
+++ src/main/java/com/skillstorm/models/Nike.java
@@ -3,11 +3,13 @@ package com.skillstorm.models;
 import java.util.Objects;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity 
 @Table(name = "nike")
 public class Nike {
 
